e1768737aea57183ce850e9f32c77bdc635a3189
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-01-16 21:50:54 +0100

    Add connection to MongoDB

--- reademails.py
+++ reademails.py
@@ -1,20 +1,37 @@
 # https://github.com/ikvk/imap_tools
+# https://pymongo.readthedocs.io/en/stable/tutorial.html
 import os
 from imap_tools import MailBox, AND
 from dotenv import load_dotenv
+import pymongo
 
 load_dotenv()
 
 # account credentials
-username = os.getenv('G_UN')
-password = os.getenv('G_PW')
+username = os.getenv('UN')
+password = os.getenv('PW')
+imapserver = os.getenv('ISV')
+mongodbcs = os.getenv('MCS')
+
+# database connection
+myclient = pymongo.MongoClient(mongodbcs)
+mydb = myclient["Cellarius"]
+collection = mydb["emails"]
 
 
-mailbox = MailBox('imap.gmail.com')
+mailbox = MailBox(imapserver)
 mailbox.login(username, password, initial_folder='INBOX')
-
+c = 0
 for msg in mailbox.fetch(AND(all=True)):
-    print("Subject:", msg.subject)
-    print("=============================================")
-    print(msg.text)
+    email = {
+        "uid": msg.uid,
+        "subject": msg.subject,
+        "from": msg.from_,
+        "to": msg.to,
+        "text": msg.text,
+        "html": msg.html
+    }
+    c = c + 1
+    x = collection.insert_one(email)
+    print ("Saved:", c, msg.uid, msg.subject)
 mailbox.logout()
