f952acd87a33a588e2cd0c46e9f0a05b367d78d9
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-01-30 14:16:41 +0100

    Create a React Webapp

--- cellarius-trainigsdata-validater-webapp/tailwind.config.js
+++ cellarius-trainigsdata-validater-webapp/tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
