f78c96521c66a6f30ac17cc5b4226803f7d39ee8
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-04-25 22:16:28 +0200

    add gpt neo to rest service

--- gpt-neo-test/rest-service/main.py
+++ gpt-neo-test/rest-service/main.py
@@ -1,3 +1,4 @@
+# cd .\gpt-neo-test\rest-service\ 
 # python3 -m uvicorn main:app
 # http://127.0.0.1:8000/docs
 from ast import In
@@ -5,8 +6,13 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 import time
-
+from subprocess import Popen, PIPE
+import sys
+from transformers import pipeline
 app = FastAPI()
+
+
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
     
 # Create a class Input  prompt, max_length=50, do_sample=True, temperature=0.9
 class Input_GPT_Neo_1_3B(BaseModel):
@@ -19,6 +25,7 @@ class Input_GPT_Neo_1_3B(BaseModel):
 class Output(BaseModel):
     output: str 
     status: str
+    error_massage: str
     loading_time_seconds: float
 
 #create a post req to fill class Input(BaseModel):
@@ -29,7 +36,18 @@ async def input_text(input: Input_GPT_Neo_1_3B):
     print(input.max_length)
     print(input.do_sample)
     print(input.temperature)
-    return Output(output=input.prompt, status="OK", loading_time_seconds=(time.time() - start_time))
+    res = generator(str(input.prompt), max_length=50, do_sample=True, temperature=0.9)
+    return Output(output=res[0]['generated_text'], status="OK", error_massage="", loading_time_seconds=(time.time() - start_time))
+
+#
+
+#prompt = "School is"
+
+#redirect console print output to a text to a string
+
+
+
+#print(res[0]['generated_text'])
 
 
 
