418435f522a9522e29d49cc2968ebd08306c7186
RomeoBhuiyan <55655775+Discord-Chan@users.noreply.github.com>
2022-05-01 20:12:58 +0200

    used a better AI

--- ai_api_docker/main.py
+++ ai_api_docker/main.py
@@ -38,4 +38,4 @@ async def root():
 
 
 #uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/cellarius/import-emails")
-uvicorn.run(app, host="0.0.0.0", port=8000)
+uvicorn.run(app, host="0.0.0.0", port=8080)
