a138bc7c92593ff3f231bad4895f8f74a9f8926e
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-05-01 15:54:12 +0200

    update rootpath

--- import-emails-docker/main.py
+++ import-emails-docker/main.py
@@ -105,7 +105,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-uvicorn.run(app, host="0.0.0.0", port=8000)
+uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/import-emails")
 
 
 
