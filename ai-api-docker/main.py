72f6494b66e84554a36f5966cfb2cce62e961a6d
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-05-31 18:18:09 +0200

    fix

--- ai-api-docker/main.py
+++ ai-api-docker/main.py
@@ -5,6 +5,11 @@ import time
 import os
 from transformers import pipeline
 
+
+os_root_path = os.environ.get('ROOT_PATH')
+print(os_root_path)
+
+
 app = FastAPI()
 
 
@@ -39,8 +44,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-os_root_path = os.environ.get('ROOT_PATH')
-print(os_root_path)
+
 
 #uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/cellarius/import-emails")
 uvicorn.run(app, host="0.0.0.0", port=8080, root_path=os_root_path)
