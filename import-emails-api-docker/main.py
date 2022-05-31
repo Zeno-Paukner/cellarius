48ffe28bcf62d8cddcb03f9cbef4f7864d9466c5
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-05-31 18:48:43 +0200

    fix roothpath

--- import-emails-api-docker/main.py
+++ import-emails-api-docker/main.py
@@ -116,7 +116,7 @@ async def root():
 
 
 #uvicorn.run(app, host="0.0.0.0", port=8000)
-uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/cellarius/import-emails")
+uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/z.paukner")
 
 
 
