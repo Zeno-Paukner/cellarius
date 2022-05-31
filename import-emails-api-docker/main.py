561b93cd954d26adb61beb71082bbefc40d4273e
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-05-31 19:11:05 +0200

    test v1

--- import-emails-api-docker/main.py
+++ import-emails-api-docker/main.py
@@ -110,19 +110,20 @@ async def import_emails(input: ImportEmails):
     scrape_emails(ImportEmails)
     return "OK"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 os_root_path = os.environ.get('ROOT_PATH')
 print(os_root_path)
+@app.get("/")
+async def root():
+    return {"message": "Hello World", "root_path": os_root_path}
+
+
 @app.get("/root_path")
 async def root():
     return {"message": os_root_path}
 
 
 #uvicorn.run(app, host="0.0.0.0", port=8000)
-uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/z.paukner/import-emails")
+uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/z.paukner")
 
 
 
